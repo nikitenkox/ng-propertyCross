5b78731c4f04ab7feec2b5ef40c17638fb55bf79
nikki <zhenya_nikitenko@ukr.net>
2017-02-07 11:49:49 +0200

    used nestoria api

--- app/components/initial-state/initial-state.component.ts
+++ app/components/initial-state/initial-state.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-
 import { Observable } from 'rxjs/Observable';
 
 import { PropertyService } from '../../services/property.service';
@@ -7,23 +6,20 @@ import { PropertyService } from '../../services/property.service';
 @Component({
     moduleId: module.id,
     selector: 'initial-state',
-    // templateUrl: './initial-state.component.html',
-    template: `
-    <h1>Wikipedia Demo</h1>
-    <p>Search after each keystroke</p>
-    <input #term (keyup)="search(term.value)"/>
-    <ul>
-      <li *ngFor="let item of items | async">{{item}}</li>
-    </ul>`,
+    templateUrl: './initial-state.component.html',
     styleUrls: ['./initial-state.component.css']
 })
 
 export class InitialStateComponent {
-    items: Observable<string[]>;
+    resp: Observable<Object[]>;
 
     constructor(private wikipediaService: PropertyService) { }
 
-    search(term: string) {
-        this.items = this.wikipediaService.search(term);
+    search(location: string) {
+        this.wikipediaService.search(location)
+            .subscribe(res => {
+                this.resp = res.response.listings;
+                console.log(res);
+            });
     }
 }
--- app/services/property.service.ts
+++ app/services/property.service.ts
@@ -1,21 +1,28 @@
 import { Injectable } from '@angular/core';
-import { URLSearchParams, Jsonp } from '@angular/http';
+import { URLSearchParams, Jsonp, Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
 
 import 'rxjs/add/operator/map';
 
 @Injectable()
 export class PropertyService {
+
     constructor(private jsonp: Jsonp) { }
-    search(term: string) {
-        let wikiUrl = 'http://en.wikipedia.org/w/api.php';
-        let params = new URLSearchParams();
-        params.set('search', term); // the user's search value
-        params.set('action', 'opensearch');
-        params.set('format', 'json');
-        params.set('callback', 'JSONP_CALLBACK');
-        // TODO: Add error handling
+
+    search(term: string): Observable<any> {
+        let api = 'http://api.nestoria.co.uk/api';
+        // let wikiUrl = 'http://en.wikipedia.org/w/api.php';
+
+        let searchParams: URLSearchParams = new URLSearchParams();
+        searchParams.set('country', 'uk');
+        searchParams.set('pretty', '1');
+        searchParams.set('action', 'search_listings');
+        searchParams.set('encoding', 'json');
+        searchParams.set('listing_type', 'buy');
+        searchParams.set('place_name', term);
+        searchParams.set('callback', 'JSONP_CALLBACK');
         return this.jsonp
-            .get(wikiUrl, { search: params })
-            .map(response => <string[]>response.json()[1]);
+            .get(api, { search: searchParams })
+            .map(response => response.json());
     }
 }
