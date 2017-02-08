1d3cfa3a471dec96053e251f113b6652e3685d84
nikki <zhenya_nikitenko@ukr.net>
2017-02-08 16:40:25 +0200

    fake city

--- app/components/search-form/search-form.component.ts
+++ app/components/search-form/search-form.component.ts
@@ -3,7 +3,6 @@ import { Router } from '@angular/router';
 
 import { PropertyService } from '../../services/property.service';
 
-
 @Component({
     moduleId: module.id,
     selector: 'search-form',
@@ -16,24 +15,22 @@ export class SearchFormComponent {
 
     constructor(private router: Router, private prorertyService: PropertyService) { }
 
-
     gotoResults(): void {
         this.prorertyService.queryTerm.next(this.searchVal);
         this.router.navigate(['/results']);
     }
 
     getLocation(event: Event) {
-        // event.preventDefault();
-        this.prorertyService.latitude.next(53.41058);
-        this.prorertyService.longitude.next(-2.97794);
+        // ********* Fake coords (Liverpool) **********
+        // this.prorertyService.latitude.next(53.41058);
+        // this.prorertyService.longitude.next(-2.97794);
         this.router.navigate(['/results']);
-        /*if (navigator.geolocation) {
+        if (navigator.geolocation) {
             navigator.geolocation.getCurrentPosition(position => {
-                console.log(`lat ${position.coords.latitude} long ${position.coords.longitude}`);
+                this.prorertyService.latitude.next(position.coords.latitude);
+                this.prorertyService.longitude.next(position.coords.longitude);
             });
-        }*/
-        /*this.prorertyService.searchByLocation(53.41058, -2.97794, 1)
-            .subscribe(res => console.log(res));*/
+        }
     }
 
 }
